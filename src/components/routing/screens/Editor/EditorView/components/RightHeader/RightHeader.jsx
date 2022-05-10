b8fcd054770394cf635e712c955f7f4adeffde3c
redoral <redmunozoral@gmail.com>
2022-05-10 11:21:57 -0700

    Updated Right Header for editor and UnplacedCharacters CSS.

--- src/components/routing/screens/Editor/EditorView/components/RightHeader/RightHeader.jsx
+++ src/components/routing/screens/Editor/EditorView/components/RightHeader/RightHeader.jsx
@@ -1,22 +1,15 @@
-import './RightHeader.css'
-
+import './RightHeader.css';
 
 export const RightHeader = () => {
-  return (
-    <div // className='header2'
-      style={{
-        height: '50px',
-        backgroundColor: 'DarkSlateGray',
-        color: 'white',
-        textAlign: 'center',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        overflow: 'hidden',
-      }}>
-      <div style={{width: '100%'}}>
-        <input type='text' placeholder='search bar' /> <button>sort</button>
-      </div>
-    </div>
-  )
-}
+	return (
+		<div className='RightHeader'>
+			<div className='SearchBarContainer'>
+				<input
+					type='text'
+					placeholder='Filter...'
+					className='SearchBar'
+				/>{' '}
+			</div>
+		</div>
+	);
+};
